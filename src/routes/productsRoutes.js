cf9f7889ff939dd0a37a74e283b27d90f44a6e2f
Bogdan-uk <656olololol@gmail.com>
2026-01-17 13:10:00 +0200

    task 1-6

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,1 +1,15 @@
+import { Router } from 'express';
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from '../controllers/productsController.js';
 
+const router = Router();
+router.get('/products', getProducts);
+router.get('/products/:productId', getProductById);
+router.post('/products', createProduct);
+router.patch('/products/:productId', updateProduct);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -6,13 +6,14 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
-
+import productsRoutes from './routes/productsRoutes.js';
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+app.use(productsRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 await connectMongoDB();
