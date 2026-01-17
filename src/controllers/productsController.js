233925644a6a743f49846d0bffd78b2839ee5209
Олександр <sulima.ab@gmail.com>
2026-01-17 12:31:06 +0200

    add router getProductById

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -5,3 +5,12 @@ export const getAllProducts = async (req, res) => {
     res.status(200).json(products);
 };
 
+export const getProductById = async (req, res) => {
+    const {productId} = req.params;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        return res.status(404).json({message : "Product not found"})
+    };
+    res.status(200).json(product);
+};
