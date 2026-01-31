a27a3738632aeefc5102cc3e3a0a36d58b2d9c7f
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-01-31 13:06:27 +0200

    make task 3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/authController.js';
+import { registerUser, loginUser } from '../controllers/authController.js';
 import { celebrate } from 'celebrate';
-import { registerUserSchema } from '../validation/authValidation.js';
+import { registerUserSchema, loginUserSchema } from '../validation/authValidation.js';
 
 const router = Router();
+
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+
+
 export default router;
