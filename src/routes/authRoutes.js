6857fb93e4158b82b7db219c4c53ff8cd4e12a3f
Bogdan-uk <656olololol@gmail.com>
2026-02-04 19:34:03 +0200

    Task 5 Refresh

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,21 @@
 import { Router } from 'express';
-import { registerUser, loginUser, logoutUser } from '../controllers/authController.js';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+} from '../controllers/authController.js';
 import { celebrate } from 'celebrate';
-import { registerUserSchema, loginUserSchema } from '../validation/authValidation.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+} from '../validation/authValidation.js';
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
-router.post('/auth/logout',logoutUser);
-
+router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 
 export default router;
